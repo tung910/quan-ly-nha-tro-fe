cb226a1c9899b227571f66934aa3fff4c43991aa
linhltph17613 <97083040+linhltph17613@users.noreply.github.com>
2022-09-30 19:46:46 +0700

    QLNTF-39 Logic code-page-data-water

--- src/pages/water/index.tsx
+++ src/pages/water/index.tsx
@@ -1,168 +1,173 @@
 /* eslint-disable no-duplicate-imports */
 /* eslint-disable no-console */
-import { Button, Checkbox, PageHeader, Tooltip } from 'antd';
-import { Link } from 'react-router-dom';
-import type { DatePickerProps } from 'antd';
-import { DatePicker, Space } from 'antd';
-import React from 'react';
-import { Select } from 'antd';
+import {
+    Button,
+    Col,
+    PageHeader,
+    Row,
+    Form,
+    DatePicker,
+    Select,
+    Input,
+} from 'antd';
 const { Option } = Select;
-import { SaveOutlined } from '@ant-design/icons';
+import { SearchOutlined, CheckOutlined, SaveOutlined } from '@ant-design/icons';
 import Table from '~/components/table';
 import { ColumnsType } from 'antd/lib/table';
 import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '~/app/hooks';
-import { fetchService } from '~/feature/service/serviceSlice';
-import { generatePriceToVND } from '~/utils/helper';
+import { getListRooms } from '~/api/room.api';
 import styles from './Water.module.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
-const columnsService: ColumnsType = [
+const ColumsDataWater: ColumnsType = [
     {
         title: 'Nhà',
-        dataIndex: 'serviceName',
+        dataIndex: 'motelId',
     },
     {
         title: 'Phòng',
-        dataIndex: 'serviceTypeName',
+        dataIndex: 'roomName',
     },
     {
         title: 'Khách thuê',
-        dataIndex: 'unitPrice',
-        render: (price) => {
-            return <>{generatePriceToVND(price)}</>;
-        },
+        dataIndex: 'customerName',
     },
     {
         title: 'CS nước cũ',
-        dataIndex: 'unitPrice',
-        render: (price) => {
-            return <>{generatePriceToVND(price)}</>;
+        dataIndex: 'OldValue',
+        render: (OldValue: any) => {
+            return (
+                <>
+                    <Input value={OldValue} />
+                </>
+            );
         },
     },
     {
         title: 'CS nước mới',
-        dataIndex: 'unitPrice',
-        render: (price) => {
-            return <>{generatePriceToVND(price)}</>;
+        dataIndex: 'NewValue',
+        render: (NewValue: any) => {
+            return (
+                <>
+                    <Input value={NewValue} />
+                </>
+            );
         },
     },
     {
         title: 'Sử dụng',
-        dataIndex: 'unitPrice',
-        render: (price) => {
-            return <>{generatePriceToVND(price)}</>;
-        },
+        dataIndex: 'UseValue',
     },
     {
         title: '',
-        width: '5%',
-        dataIndex: '_id',
-        render: (id) => {
+        dataIndex: 'recond',
+        render: (text: any, recond: any) => {
             return (
-                <Space>
-                    <Tooltip title='lưu'>
-                        <Link to={'edit/' + id}>
-                            <Button
-                                type='primary'
-                                icon={<SaveOutlined />}
-                            ></Button>
-                        </Link>
-                    </Tooltip>
-                </Space>
+                <>
+                    <Button type='primary' icon={<SaveOutlined />}>
+                        Lưu
+                    </Button>
+                </>
             );
         },
     },
 ];
 
 const WaterPage = () => {
-    const dispatch = useAppDispatch();
-    const serviceStore = useAppSelector((state) => state.service.value);
-    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-        setSelectedRowKeys(newSelectedRowKeys);
-    };
-    const rowSelection = {
-        type: selectedRowKeys,
-        onChange: onSelectChange,
-    };
-    const handleChange = (value: string) => {
-        console.log(`selected ${value}`);
-    };
-    const onChange: DatePickerProps['onChange'] = (date, dateString) => {
-        console.log(date, dateString);
-    };
+    const [dataPower, setDataPower] = useState([]);
     useEffect(() => {
-        dispatch(fetchService());
-    }, [dispatch]);
-
+        const listMotelRoom = async () => {
+            const { data } = await getListRooms();
+            const newData = data.map((item: any) => {
+                const result = {
+                    ...item,
+                    OldValue: 0,
+                    NewValue: 0,
+                    UseValue: 0,
+                };
+                return result;
+            });
+            setDataPower(newData);
+        };
+        listMotelRoom();
+    }, []);
     return (
         <div>
             <div>
-                <PageHeader ghost={true} title='Chỉ số nước'></PageHeader>
+                <PageHeader
+                    className={cx('header-top')}
+                    title='Chỉ số nước'
+                    extra={[
+                        <Button icon={<SearchOutlined />} key={1}>
+                            Xem
+                        </Button>,
+                        <Button type='primary' icon={<CheckOutlined />} key={2}>
+                            Lưu thông tin
+                        </Button>,
+                    ]}
+                ></PageHeader>
             </div>
             <div className=''>
-                <div className={cx('grid-date')}>
-                    <div className=''>
-                        <label>
-                            Tháng/năm: &nbsp;
-                            <Space direction='vertical'>
-                                <DatePicker onChange={onChange} />
-                            </Space>
-                        </label>
-                    </div>
-                    <div className=''>
-                        <label>
-                            Kì: &nbsp;
-                            <Select
-                                defaultValue='lucy'
-                                style={{ width: 150 }}
-                                onChange={handleChange}
+                <div className={cx('header-bottom')}>
+                    <Row gutter={[8, 8]}>
+                        <Col span={6}>
+                            <Form.Item label={<>Tháng/năm</>} colon={false}>
+                                <DatePicker name='date' />
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Form.Item label={<>Kỳ</>} colon={false}>
+                                <Select
+                                    style={{ width: 150 }}
+                                    defaultValue={1}
+                                    showSearch
+                                >
+                                    <Option value={1}>Tất cả</Option>
+                                    <Option value={2}>Kỳ 30</Option>
+                                    <Option value={3}>Kỳ 15</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Form.Item label={<>Nhà</>} colon={false}>
+                                <Select
+                                    style={{ width: 150 }}
+                                    defaultValue={1}
+                                    showSearch
+                                >
+                                    <Option value={1}>Tất cả</Option>
+                                    <Option value={2}>Kỳ 30</Option>
+                                    <Option value={3}>Kỳ 15</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                        <Col span={6}>
+                            <Form.Item
+                                label={<>Trạng thái nhà</>}
+                                colon={false}
                             >
-                                <Option value='jack'>1</Option>
-                                <Option value='lucy'>2</Option>
-                                <Option value='lucy'>3</Option>
-                                <Option value='lucy'>Tất cả</Option>
-                            </Select>
-                        </label>
-                    </div>
-                    <div className=''>
-                        <label>
-                            Nhà: &nbsp;
-                            <Select
-                                defaultValue='lucy'
-                                style={{ width: 150 }}
-                                onChange={handleChange}
-                            >
-                                <Option value='jack'> Tầng 1</Option>
-                                <Option value='lucy'> Tầng 2</Option>
-                                <Option value='lucy'> Tầng 3</Option>
-                                <Option value='lucy'>Tất cả</Option>
-                            </Select>
-                        </label>
-                    </div>
-                    <div className=''>
-                        <label>
-                            Trạng thái phòng: &nbsp;
-                            <Select
-                                defaultValue='lucy'
-                                style={{ width: 150 }}
-                                onChange={handleChange}
-                            >
-                                <Option value='jack'>Đã cho thuê</Option>
-                                <Option value='lucy'>Còn trống</Option>
-                                <Option value='lucy'>Tất cả</Option>
-                            </Select>
-                        </label>
-                    </div>
+                                <Select
+                                    style={{ width: 150 }}
+                                    defaultValue={1}
+                                    showSearch
+                                >
+                                    <Option value={1}>Tất cả</Option>
+                                    <Option value={2}>Kỳ 30</Option>
+                                    <Option value={3}>Kỳ 15</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
                 </div>
-                <b>Lưu ý:</b>
-                <br></br> - Bạn phải gán dịch vụ thuộc loại nước cho khách thuê
-                trước thì phần chỉ số này mới được tính cho phòng đó khi tính
-                tiền.<br></br>- Đối với lần đầu tiên sử dụng phần mềm bạn sẽ
-                phải nhập chỉ số cũ và mới cho tháng sử dụng đầu tiên, các tháng
-                tiếp theo phần mềm sẽ tự động lấy chỉ số mới tháng trước làm chỉ
-                số cũ tháng sau.
+                <div className=''>
+                    <b>Lưu ý:</b>
+                    <br></br> - Bạn phải gán dịch vụ thuộc loại nước cho khách
+                    thuê trước thì phần chỉ số này mới được tính cho phòng đó
+                    khi tính tiền.<br></br>- Đối với lần đầu tiên sử dụng phần
+                    mềm bạn sẽ phải nhập chỉ số cũ và mới cho tháng sử dụng đầu
+                    tiên, các tháng tiếp theo phần mềm sẽ tự động lấy chỉ số mới
+                    tháng trước làm chỉ số cũ tháng sau.
+                </div>
                 <div className=''>
                     <b>
                         {' '}
@@ -173,11 +178,7 @@ const WaterPage = () => {
                 <br></br>
             </div>
             <div>
-                <Table
-                    columns={columnsService}
-                    dataSource={serviceStore}
-                    rowSelection={rowSelection}
-                />
+                <Table columns={ColumsDataWater} dataSource={dataPower} />
             </div>
         </div>
     );
