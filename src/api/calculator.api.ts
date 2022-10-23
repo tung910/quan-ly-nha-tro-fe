bfe2ac685635e0690f1c3a215eb7acab571819f6
long2512 <buidanglong02@gmail.com>
2022-10-23 09:37:28 +0700

    QLNTF-55 Enter the amount earned

--- src/api/calculator.api.ts
+++ src/api/calculator.api.ts
@@ -13,3 +13,6 @@ export const CalculatorMoney = (data: any) => {
 export const deleteCalculator = (id: string) => {
     return instance.delete(`/calculator-money/delete/${id}`);
 };
+export const paymentMoney = (data: any) =>{
+    return instance.put(`/calculator-money/payment/${data._id}`,{data})
+}
